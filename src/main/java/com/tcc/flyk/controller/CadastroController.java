b05e856ee1c8abc55bc71fb0f64ec87325670253
Fabiana Santos <fabicreds@gmail.com>
2016-05-11 19:45:30 -0300

    inserção pastas do build path

--- src/main/java/com/tcc/flyk/controller/CadastroController.java
+++ src/main/java/com/tcc/flyk/controller/CadastroController.java
@@ -5,15 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value = "/cadastro")
 public class CadastroController {
 	
-	@RequestMapping(value = "/cadastro", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public String iniciarTelaCadastro() {
 
 		return "cadastro";
 	}
 	
-	@RequestMapping(value = "/cadastro", method = RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.POST)
 	public String realizarCadastro() {
 		System.out.println("Cadastro de usuário");
 		return "cadastro";
