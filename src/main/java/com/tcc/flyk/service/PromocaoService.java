c6a406e0f50cb7d09285abd186c2840c10db9d04
Yasmin Nogueira Spadaro Cropanisi <yasmin@aquarius.cpqd.com.br>
2016-05-11 20:20:17 -0300

    Cadastrar promocao e tag lig

---
+++ src/main/java/com/tcc/flyk/service/PromocaoService.java
@@ -0,0 +1,16 @@
+package com.tcc.flyk.service;
+
+import org.springframework.stereotype.Service;
+
+import com.tcc.flyk.entity.Promocao;
+@Service
+public class PromocaoService {
+	
+	public void cadastrarPromocao(Promocao prom)
+	{
+		Promocao p= prom;
+		
+		//TODO: Cadastrar no doc de promocao
+	}
+
+}
