9397f1f6c27083d17c7488e9383119ee5c316385
Fabiana Santos <fabicreds@gmail.com>
2016-05-10 22:12:28 -0300

    adicao das telas e angular

---
+++ src/main/java/com/tcc/flyk/controller/InicioController.java
@@ -0,0 +1,38 @@
+package com.tcc.flyk.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tcc.flyk.service.LoginService;
+
+@Controller
+public class InicioController {
+
+	@Autowired
+	LoginService loginService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String telaPrincipal() {
+
+		return "index";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String iniciarTelaLogin() {
+
+		return "login";
+	}
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String telaHome() {
+
+		return "home";
+	}
+
+
+
+}
