6dcea1b17962492ee0c2c3aaf7773e74480ba8d4
fabicreds <fabicreds@gmail.com>
2016-06-06 09:21:48 -0300

    Commit da Yasmin

---
+++ src/main/java/com/tcc/flyk/entity/Preco.java
@@ -0,0 +1,24 @@
+package com.tcc.flyk.entity;
+
+public class Preco {
+	
+	private Float valor;
+	private String categoria;
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public Float getValor() {
+		return valor;
+	}
+
+	public void setValor(Float valor) {
+		this.valor = valor;
+	}
+
+}
