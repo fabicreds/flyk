10ce53e2d300d4c5fe1daf4d3057dbc2f1fda2fd
fabicreds <fabicreds@gmail.com>
2016-09-19 10:20:12 -0300

    Ajustes de Commits

--- src/main/webapp/resources/angular/app/controllers/profilePage.js
+++ src/main/webapp/resources/angular/app/controllers/profilePage.js
@@ -4,15 +4,15 @@ flyk.controller("profilePageCtrl", function ($scope, $rootScope, $location, $htt
     	$location.path('/profilePageEdit');
     }
     
-    if($rootScope.data.tipoCadastro == "2")
-    	
-    {
-    	$rootScope.data.tipoCadastro = true;
-    }
-    else
-    {
-    	$rootScope.data.tipoCadastro = false;
-    }
+//    if($rootScope.data.tipoCadastro == "2")
+//    	
+//    {
+//    	$rootScope.data.tipoCadastro = true;
+//    }
+//    else
+//    {
+//    	$rootScope.data.tipoCadastro = false;
+//    }
     
     
    /* if($rootScope.telefone.categoria == "FIXO")
@@ -120,4 +120,37 @@ flyk.controller("profilePageCtrl", function ($scope, $rootScope, $location, $htt
     $scope.$on("fileProgress", function(e, progress) {
         $scope.progress = progress.loaded / progress.total;
     });
+    
+    
+    $scope.init = function () {
+    	$scope.showProfilePage();
+    }
+    
+    $scope.showProfilePage = function() {
+		if ($rootScope.data == null || ($rootScope.data != null && $rootScope.data.nome == null)) {
+			$http({
+				url : 'profilePage',
+				method : "POST",
+				data : {
+					'usuario' : localStorage.getItem("usuarioLogado"),
+					'idUsuario' : localStorage.getItem("idUsuarioLogado"),
+					'tipoUsuario' : localStorage.getItem("tipoUsuarioLogado")
+				}
+			}).then(function(response) {
+				if (response.data.retorno != "erro") {
+					$rootScope.usuarioLogado = response.data.usuario;
+					$rootScope.tipoUsuarioLogado = response.data.tipoCadastro;
+					$rootScope.idUsuarioLogado = response.data.cliente.id;
+					$rootScope.data = response.data.cliente;
+					$location.path('/profilePage');
+				} else {
+					$location.path('/profilePage');
+				}
+			}, function(response) {
+
+			});
+		} else {
+			$location.path('/profilePage');
+		}
+	};
 });
